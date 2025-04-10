f8bd16871a2ce3d58e21c547bc628632078850bf
Sam Fonseca <samuel.fonseca@protonmail.com>
2025-04-10 13:07:04 -0400

    Adds Cypress configuration

---
+++ cypress/e2e/home.cy.ts
@@ -0,0 +1,12 @@
+describe('My First Test', () => {
+    it('headings are set as expected', () => {
+        cy.visit('/')
+        cy.contains('h1.green', 'You did it!').should('be.visible')
+
+        cy.contains('h3', 'Documentation').should('be.visible')
+        cy.contains('h3', 'Tooling').should('be.visible')
+        cy.contains('h3', 'Ecosystem').should('be.visible')
+        cy.contains('h3', 'Community').should('be.visible')
+        cy.contains('h3', 'Support Vue').should('be.visible')
+    })
+})
